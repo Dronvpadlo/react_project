bebf13dabb7f1a7ca12312838fd8ff5c9cda41a7
User <doronyuk714@mail.com>
2024-06-25 16:28:50 +0300

    Complete Project

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,98 @@ const App: FC = () => {
   return (
     <>
       <Character name={'Arhtur Morgan'}
-                 image={'https://www.pngfind.com/pngs/m/214-2140558_the-van-der-linde-red-dead-redemption-2.png'}>
+                 image={'https://pbs.twimg.com/media/Dmckag_XcAAnmyI?format=jpg'}>
           Arthur Morgan is a central character and the primary protagonist of Red Dead Redemption 2.
       </Character>
       <Character name={'John Marston'}
-                 image={'https://www.pngfind.com/pngs/m/500-5005601_red-dead-redemption-red-dead-redemption-2-grand.png'}>
+                 image={'https://static0.gamerantimages.com/wordpress/wp-content/uploads/Red-Dead-Redemption-2-new-character-art-John-Marston.jpg'}>
           John Marston is a recurring character in the Red Dead series, appearing as a central character and the primary protagonist of Red Dead Redemption and its non-canon Undead Nightmare expansion pack, and is a central character and the secondary protagonist of Red Dead Redemption 2.
       </Character>
       <Character name={'Dutch Van Der Linde'}
-                 image={'https://www.pngfind.com/pngs/m/392-3927236_red-dead-redemption-2-dutch-png-download-dutch.png'}>
-          Dutch van der Linde is a recurring character in the Red Dead series, appearing as a central character and the secondary antagonist of Red Dead Redemption, as well as a central character in Red Dead Redemption 2.
+                 image={'https://www.rdr2.org/wp-content/uploads/2018/09/dutchvanderlinde.jpg'}>
+          Dutch Van Der Linde first appeared as a primary antagonist in the 2010 video game Red Dead Redemption, and returns in as a primary character in Red Dead Redemption 2. As a legendary outlaw, Dutch is opposed by the authorities and rival gangs.
       </Character>
+        <Character name={'Hosea Matthews'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/hosea-matthews-1200x675.jpg'}>
+            Hosea Matthews is a major character featured in Red Dead Redemption 2.
+        </Character>
       <Character name={'Xavier Escuella'}
-                 image={'https://www.pngfind.com/pngs/m/285-2851164_npxbl-javier-escuella-hd-png-download.png'}>
+                 image={'https://oyster.ignimgs.com/mediawiki/apis.ign.com/red-dead-redemption-2/7/77/Javier_Escuell.jpg'}>
         Javier Escuella is a recurring character in the Red Dead series, appearing as a central character and a major antagonist in Red Dead Redemption, and as a major character in Red Dead Redemption 2.
       </Character>
+        <Character name={'Bill Williamson'}
+                   image={'https://oyster.ignimgs.com/mediawiki/apis.ign.com/red-dead-redemption-2/2/2b/Bill_Williamson.jpg'}>
+            Bill Williamson first appeared as a secondary antagonist in the 2010 video game Red Dead Redemption and returns as a supporting character in Red Dead Redemption 2, where he is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Micah Bell'}
+                   image={'https://static1.thegamerimages.com/wordpress/wp-content/uploads/2019/07/micah-bell.jpg'}>
+            Micah Bell is a supporting character in, and main antagonist of, Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
       <Character name={'Sean Macguire'}
-                 image={'https://www.pngfind.com/pngs/m/392-3927620_read-dead-rdr-2-red-dead-redemption-ii.png'}>
+                 image={'https://oyster.ignimgs.com/mediawiki/apis.ign.com/red-dead-redemption-2/2/2d/Sean_Macguire.jpg'}>
         Sean MacGuire is a recurring character in the Red Dead series, appearing as a supporting character in Red Dead Redemption 2 and as a stranger in Red Dead Online.
       </Character>
+        <Character name={'Sadie Adler'}
+                   image={'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/01/red-dead-redemption-2-sadie-adler.jpg'}>
+            Sadie Adler first appears as a supporting character in Red Dead Redemption 2. She was a member of Dutch’s Gang and worked together with Arthur Morgan. She also appears as a character in Red Dead Online.
+        </Character>
       <Character name={'Karen Jones'}
-                 image={'https://www.pngfind.com/pngs/m/306-3068880_npxbl-red-dead-redemption-2-karen-hd-png.png'}>
+                 image={'https://www.rdr2.org/wp-content/uploads/2018/11/Karen_Jone.jpg'}>
         Karen Jones is a supporting character featured in Red Dead Redemption 2.
       </Character>
+        <Character name={'Charles Smith'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/charles-smith.jpg'}>
+            Charles Smith first appears as a supporting character in Red Dead Redemption 2. He was a member of Dutch’s Gang and worked together with Arthur Morgan.
+        </Character>
+        <Character name={'Abigail Roberts'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/09/abigail.jpg'}>
+            Abigail Marston, née Roberts, is John Marston’s wife and a central character in the Red Dead franchise. She is a major character in Red Dead Redemption and the Undead Nightmare DLC and a central supporting character in Red Dead Redemption 2. With the release of the Liars and Cheats DLC pack, Abigail Marston’s model can also be accessed in Red Dead Redemption multiplayer mode in the Redemption section of the Outfitter.
+        </Character>
+        <Character name={'Uncle'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Uncle_Red_Dead_Redemption_2.jpg'}>
+            Uncle is a central character in the Red Dead franchise. He is a major character in the original Red Dead Redemption and a supporting character in the Undead Nightmare DLC and Red Dead Redemption 2.
+        </Character>
+        <Character name={'Lenny Summers'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Lenny_Summers_Artwork.jpg'}>
+            Lenny Summers is a supporting character in Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Mary-Beth Gaskill'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/12/mary-beth.jpg'}>
+            Mary-Beth Gaskill is a supporting character in Red Dead Redemption 2. She is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Leopold Strauss'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Leopold_Strauss.jpg'}>
+            Leopold Strauss is a supporting character in Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Pearson'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Pearson.jpg'}>
+            Pearson is a supporting character in Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Josiah Trelawny'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Josiah_Trelawny.jpg'}>
+            Josiah Trelawny is a supporting character in Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Reverend Swanson'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Reverend_Swanson.jpg'}>
+            Reverend Swanson is a supporting character in Red Dead Redemption 2. He is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Susan Grimshaw'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Susan_Grimshaw.jpg'}>
+            Susan Grimshaw is a supporting character in Red Dead Redemption 2. She is the arbiter of the Van der Linde Gang.
+        </Character>
+        <Character name={'Tilly Jackson'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/Tilly_Jackson.jpg'}>
+            Tilly Jackson is a supporting character in Red Dead Redemption 2. She is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'Molly O’Shea'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/11/molly-oshea-1200x675.jpg'}>
+            Molly O’Shea is a supporting character in Red Dead Redemption 2. She is a member of the Van der Linde Gang.
+        </Character>
+        <Character name={'John “Jack” Marston'}
+                   image={'https://www.rdr2.org/wp-content/uploads/2018/12/jack-marston-1200x675.jpg'}>
+            John “Jack” Marston, Jr. appears as a playable character in Red Dead Redemption and as supporting character in Red Dead Redemption 2. His character model also appears in Red Dead Redemption multiplayer as a selectable skin provided the Liars and Cheats DLC is installed. He is the son of John Marston and Abigail Roberts and traveled with the Van Der Linde Gang for a period of time during 1899.
+        </Character>
+
     </>
   );
 }

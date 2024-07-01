642bb3ad497675944a6aaf7f34cc6621961eb036
User <doronyuk714@mail.com>
2024-07-01 23:55:55 +0300

    progress...

--- src/components/user-component/userComponent.tsx
+++ src/components/user-component/userComponent.tsx
@@ -1,78 +1,77 @@
 import React, {FC} from 'react';
 import {IUser} from "../../models/IUser";
-import {getPostsOFUser} from "../../services/api.service";
-
+import styles from './user.module.css'
 interface IProps{
     user: IUser
         getPosts: (id: number) => void
 }
 const UserComponent: FC<IProps> = ({user, getPosts}) => {
     return (
-        <div>
-            <div>{user.firstName}</div>
-            <div>{user.lastName}</div>
-            <div>{user.maidenName}</div>
-            <div>{user.age}</div>
-            <div>{user.gender}</div>
-            <div>{user.email}</div>
-            <div>{user.phone}</div>
-            <div>{user.username}</div>
-            <div>{user.password}</div>
-            <div>{user.birthDate}</div>
+        <div className={styles.section}>
+            <div>Name: {user.firstName}{user.lastName}</div>
+            <div>Maiden Name{user.maidenName}</div>
+            <div>Age: {user.age}</div>
+            <div>Gender: {user.gender}</div>
+            <div>Email: {user.email}</div>
+            <div>Phone: {user.phone}</div>
+            <div>Username: {user.username}</div>
+            <div>Password: {user.password}</div>
+            <div>Birthday: {user.birthDate}</div>
             <img src={user.image} alt="User Image"/>
-            <div>{user.bloodGroup}</div>
-            <div>{user.height}</div>
-            <div>{user.weight}</div>
-            <div>{user.eyeColor}</div>
-            <div>{user.hair.color} {user.hair.type}</div>
-            <div>{user.ip}</div>
-            <div>
-                <div>Adress: {user.address.address}</div>
-                <div>{user.address.city}</div>
-                <div>{user.address.state}</div>
-                <div>{user.address.stateCode}</div>
-                <div>{user.address.postalCode}</div>
-                <div>{user.address.coordinates.lat}</div>
-                <div>{user.address.coordinates.lng}</div>
-                <div>{user.address.country}</div>
-            </div>
-            <div>{user.macAddress}</div>
-            <div>{user.university}</div>
-            <div>
-                <div>{user.bank.cardExpire}</div>
-                <div>{user.bank.cardNumber}</div>
-                <div>{user.bank.cardType}</div>
-                <div>{user.bank.currency}</div>
-                <div>{user.bank.iban}</div>
-            </div>
-            <div>
-                <div>{user.company.department}</div>
-                <div>{user.company.name}</div>
-                <div>{user.company.title}</div>
-                <div>{user.company.address.address}</div>
-                <div>{user.company.address.city}</div>
-                <div>{user.company.address.state}</div>
-                <div>{user.company.address.stateCode}</div>
-                <div>{user.company.address.postalCode}</div>
-                <div>{user.company.address.coordinates.lat}</div>
-                <div>{user.company.address.coordinates.lng}</div>
-                <div>{user.company.address.country}</div>
-            </div>
-            <div>{user.ein}</div>
-            <div>{user.ssn}</div>
-            <div><div>{user.userAgent}</div>
-                <div>{user.crypto.coin}</div>
-                <div>{user.crypto.wallet}</div>
-                <div>{user.crypto.network}</div>
-            </div>
-            <div>{user.role}</div>
+            <div>Blood Group: {user.bloodGroup}</div>
+            <div>Height: {user.height}</div>
+            <div>Weight: {user.weight}</div>
+            <div>Eyes Color: {user.eyeColor}</div>
+            <div>Hair: {user.hair.color} {user.hair.type}</div>
+            <div>IP: {user.ip}</div>
+
             <button onClick={()=>{
                     getPosts(user.id)
 
             }}>Posts</button>
-                <hr/>
         </div>
     );
 };
 
 export default UserComponent;
+
+//<div className={styles.address}>
+//                 <div>Adress: {user.address.address}</div>
+//                 <div>{user.address.city}</div>
+//                 <div>{user.address.state}</div>
+//                 <div>{user.address.stateCode}</div>
+//                 <div>{user.address.postalCode}</div>
+//                 <div>{user.address.coordinates.lat}</div>
+//                 <div>{user.address.coordinates.lng}</div>
+//                 <div>{user.address.country}</div>
+//             </div>
+//             <div>{user.macAddress}</div>
+//             <div>{user.university}</div>
+//             <div>
+//                 <div>{user.bank.cardExpire}</div>
+//                 <div>{user.bank.cardNumber}</div>
+//                 <div>{user.bank.cardType}</div>
+//                 <div>{user.bank.currency}</div>
+//                 <div>{user.bank.iban}</div>
+//             </div>
+//             <div>
+//                 <div>{user.company.department}</div>
+//                 <div>{user.company.name}</div>
+//                 <div>{user.company.title}</div>
+//                 <div>{user.company.address.address}</div>
+//                 <div>{user.company.address.city}</div>
+//                 <div>{user.company.address.state}</div>
+//                 <div>{user.company.address.stateCode}</div>
+//                 <div>{user.company.address.postalCode}</div>
+//                 <div>{user.company.address.coordinates.lat}</div>
+//                 <div>{user.company.address.coordinates.lng}</div>
+//                 <div>{user.company.address.country}</div>
+//             </div>
+//             <div>{user.ein}</div>
+//             <div>{user.ssn}</div>
+//             <div><div>{user.userAgent}</div>
+//                 <div>{user.crypto.coin}</div>
+//                 <div>{user.crypto.wallet}</div>
+//                 <div>{user.crypto.network}</div>
+//             </div>
+//             <div>{user.role}</div>
--- src/components/users-componet/usersComponent.tsx
+++ src/components/users-componet/usersComponent.tsx
@@ -4,7 +4,7 @@ import {IUser} from "../../models/IUser";
 import {getAllUsers, getPostsOFUser} from "../../services/api.service";
 import {IPost} from "../../models/IPost";
 import PostsComponent from "../posts-component/postsComponent";
-
+import styles from './users.module.css'
 const UsersComponent = () => {
     const [usersList, setUsersList] = useState<IUser[]>([]);
     const [posts, setPosts] = useState<IPost[]>([])
@@ -17,19 +17,22 @@ const getPosts = (id: number) =>{
     getPostsOFUser(id).then(posts => setPosts([...posts.posts]));
 }
     return (
-        <div>
+        <div className={styles.section}>
             <div>
+            <div className={styles.flex}>
                 {
                     usersList.map((user) => (<UserComponent
                         key={user.id}
                         user={user}
                         getPosts={getPosts}
 
-                        />))
+                    />))
                 }
+
             </div>
             <div>
                 <PostsComponent posts={posts}/>
+            </div>
             </div>
         </div>
     );

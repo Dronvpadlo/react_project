de255c29c80da58b88298cdf13f3dd09f509226b
User <doronyuk714@mail.com>
2024-07-17 23:46:34 +0300

    Users Page Stylizated

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import UsersComponent from "../../components/Users/UsersComponent/UsersComponent";
-
+import styles from './UsersPage.module.css'
 
 const UsersPage = () => {
     return (
-        <div>
+        <div className={styles.bgc}>
             <UsersComponent/>
         </div>
     );

41a6fadec62e9dd3a1c95eddac6bcf3d290222a5
User <doronyuk714@mail.com>
2024-07-17 23:20:06 +0300

    complete

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
-
 const UserPage = () => {
 
     const {id} = useParams();
     return (
-        <div>
+        <div >
             UserPage {id}
         </div>
     );

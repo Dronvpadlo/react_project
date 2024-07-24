e4ec51ae364a227a2ea309ef5e218943b37bb6b4
User <doronyuk714@mail.com>
2024-07-24 21:28:52 +0300

    add memo to header and footer

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <ul>
                 <li>Home</li>
                 <li>Log In</li>
                 <li>Sing In</li>
                 <li>Cars</li>
-            </ul>
         </div>
     );
 };

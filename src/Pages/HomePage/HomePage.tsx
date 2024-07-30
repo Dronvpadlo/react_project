07b5a280b35fc31003a8e0a7710857e556b25fd2
User <doronyuk714@mail.com>
2024-07-30 22:24:56 +0300

    complete all styles

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './HomePage.module.css'
+import {NavLink} from "react-router-dom";
 const HomePage = () => {
     return (
         <div className={styles.homeSection}>
-            <img className={styles.homeSection} src="https://wallpaper.forfun.com/fetch/61/61fb7cd39461bb062d00b989356855e3.jpeg" alt="MainImage"/>
+            <div className={styles.homeNav}>
+                <h3>Welcome!!!</h3>
+                <NavLink className={styles.linksStyle} to={'/home'}>Home</NavLink><br/>
+                <NavLink className={styles.linksStyle} to={'/reg'}>Sing In</NavLink><br/>
+                <NavLink className={styles.linksStyle} to={'/auth'}>Log In</NavLink><br/>
+                <NavLink className={styles.linksStyle} to={'/cars'}>Cars List</NavLink><br/>
+            </div>
         </div>
     );
 };

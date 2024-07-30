56a2beb9fc31fcf8e86189fee590f54b7ec4c5bc
User <doronyuk714@mail.com>
2024-07-30 21:56:25 +0300

    Add styles to all

--- src/Components/CarComponent/CarComponent.tsx
+++ src/Components/CarComponent/CarComponent.tsx
@@ -1,14 +1,17 @@
 import React, {FC} from 'react';
 import {ICars} from "../../Models/ICars";
-
+import styles from './CarComponent.module.css'
 
 const CarComponent:FC<ICars> = ({id, year, brand, price }) => {
     return (
-        <div>
-            ID: {id}<br/>
-            Brand: {brand}<br/>
-            Year: {year}<br/>
-            Price: {price}
+        <div className={styles.mainBlock}>
+            <div className={styles.carBlock}>
+                ID: {id}<br/>
+                Brand: {brand}<br/>
+                Year: {year}<br/>
+                Price: {price}<br/>
+            </div>
+
         </div>
     );
 };
--- src/Components/CarsComponent/CarsComponent.tsx
+++ src/Components/CarsComponent/CarsComponent.tsx
@@ -1,19 +1,23 @@
 import React, {FC} from 'react';
 import {ICars} from "../../Models/ICars";
 import CarComponent from "../CarComponent/CarComponent";
-
+import styles from './CarsComponent.module.css'
 
 interface IProps {
     cars: ICars[]
 }
 const CarsComponent:FC<IProps> = ({cars}) => {
     return (
-        <div>
-            {
-                cars.map(({brand, price, id, year}, index) => <CarComponent key={index} id={id} brand={brand} price={price} year={year}/>
+        <div className={styles.mainSection}>
 
+            {
+                cars.map(({brand, price, id, year}, index) => <CarComponent key={index} id={id} brand={brand}
+                                                                            price={price} year={year}/>
                 )}
+            <button className={styles.button}>Prev</button>
+            <button className={styles.button}>Next</button>
         </div>
+
     );
 };
 

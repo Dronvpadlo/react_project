da5a43a0e2a58ae50b14c62f9a87366f00acf6b3
User <doronyuk714@mail.com>
2024-07-30 22:26:17 +0300

    complete all styles

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -6,7 +6,6 @@ const HomePage = () => {
         <div className={styles.homeSection}>
             <div className={styles.homeNav}>
                 <h3>Welcome!!!</h3>
-                <NavLink className={styles.linksStyle} to={'/home'}>Home</NavLink><br/>
                 <NavLink className={styles.linksStyle} to={'/reg'}>Sing In</NavLink><br/>
                 <NavLink className={styles.linksStyle} to={'/auth'}>Log In</NavLink><br/>
                 <NavLink className={styles.linksStyle} to={'/cars'}>Cars List</NavLink><br/>

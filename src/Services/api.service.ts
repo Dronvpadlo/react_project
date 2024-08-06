149c88a279f7f97fdf7efaeaa01dc6f74a9a1196
User <doronyuk714@mail.com>
2024-08-06 23:34:07 +0300

    Init Structure

--- src/Services/api.service.ts
+++ src/Services/api.service.ts
@@ -1,12 +1,29 @@
 import axios from "axios";
+import {IUsersModel} from "../Models/IUsersModel";
+import {IPostsModel} from "../Models/IPostsModel";
+import {ICommentsModel} from "../Models/ICommentsModel";
 
 const AxiosInstance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/',
     headers: {}
 })
 
-const userService.getUsers = async ()=>{
-    await AxiosInstance.get('/users')
+const userService = {
+    getUsers: async (): Promise<IUsersModel[]> => {
+        return await AxiosInstance.get('/users')
+    }
 }
 
-export {userService}
+const postService = {
+    getPosts: async ():Promise<IPostsModel[]> =>{
+        return await AxiosInstance.get('/posts')
+    }
+}
+
+const commentService = {
+    getComments: async ():Promise<ICommentsModel[]> =>{
+        return await AxiosInstance.get('/comments')
+}
+}
+
+export {userService, postService, commentService}

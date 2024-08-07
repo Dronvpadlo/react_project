7465994a58442a20b1afa1ef6f53248985146bfe
User <doronyuk714@mail.com>
2024-08-07 16:33:05 +0300

    Complete HW without CSS

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import styles from './Header.module.css'
 
 const Header = () => {
     return (
-        <div>
-            <NavLink to={'/home'}>Home</NavLink>
-            <NavLink to={'/users'}>Users</NavLink>
-            <NavLink to={'/usersPosts'}>Users with posts</NavLink>
-            <NavLink to={'/posts'}>Posts</ NavLink>
-            <NavLink to={'/postsComments'}>Posts with comments</NavLink>
-            <NavLink to={'/comments'}>Comments</NavLink>
+        <div className={styles.headerSection}>
+            <NavLink className={styles.link} to={'/home'}>Home</NavLink>
+            <NavLink className={styles.link} to={'/users'}>Users</NavLink>
+            <NavLink className={styles.link} to={'/usersPosts'}>Users with posts</NavLink>
+            <NavLink className={styles.link} to={'/posts'}>Posts</ NavLink>
+            <NavLink className={styles.link} to={'/postsComments'}>Posts with comments</NavLink>
+            <NavLink className={styles.link} to={'/comments'}>Comments</NavLink>
             <hr/>
         </div>
     );
